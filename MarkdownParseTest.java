0931870d4547ca7a083083287ab98e8cda47277e
mlin1026 <mike911026@gmail.com>
2022-01-27 14:22:29 -0800

    Update MarkdownParseTest.java
    Adding own testing methods.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,14 +1,28 @@
 import static org.junit.Assert.*;
-import  org.junit.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+    assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void getLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("https://something.com", links.get(0));
+        assertEquals("some-page.html", links.get(1));
     }
 }
 
 /*
+Windows
 javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 */
