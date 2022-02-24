de749a41f026e08eadf060c6c8610af649244ef1
mlin1026 <mike911026@gmail.com>
2022-02-24 15:55:59 -0800

    Lab 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -53,5 +53,31 @@ public class MarkdownParseTest {
         assertEquals("https://something.com", links.get(0));
         assertEquals("some-page.html", links.get(1));
     }
+    @Test
+    public void getSnippet() throws IOException {
+        Path fileName = Path.of("snippet_1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("`google.com", links.get(0));
+    }
+
+    @Test
+    public void getSnippet2() throws IOException {
+        Path fileName = Path.of("snippet_2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("a.com", links.get(0));
+        assertEquals("example.com", links.get(2));
+        assertEquals("a.com(())", links.get(1));
+    }
+
+    @Test
+    public void getSnippet3() throws IOException {
+        Path fileName = Path.of("snippet_3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("https://ucsd-cse15l-w22.github.io/", links.get(0));
+    }
+
 }
 
